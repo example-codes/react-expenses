7e3c0b3d666add1f55eaccd2939c7b08617af74b
Muhammad Ahmar Hilal <ahmarcode@gmail.com>
2023-08-02 15:57:52 +0530

    add dynamic listing

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,23 +1,19 @@
-import React, { useState } from "react";
+import React from 'react';
 
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
-import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const [_amount, setAmount] = useState(props.amount);
-
-
   return (
-    <Card className="expense-item">
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${_amount}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </Card>
   );
-};
+}
 
 export default ExpenseItem;
