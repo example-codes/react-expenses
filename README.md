98fceb3cd5f9b8f2fd256f093ecfe73a6f69d6c6
Muhammad Ahmar Hilal <ahmarcode@gmail.com>
2023-08-05 01:49:16 +0530

    Add backend API with localhost:3000

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+backend app: https://github.com/example-codes/local-internet-latency
+url http://localhost:3000/data
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
-
-
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 const DUMMY_EXPENSES = [
   {
-    id: 'e1',
-    title: 'Toilet Paper',
+    id: "e1",
+    title: "Toilet Paper",
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
-    id: 'e3',
-    title: 'Car Insurance',
+    id: "e3",
+    title: "Car Insurance",
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
   {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
+    id: "e4",
+    title: "New Desk (Wooden)",
     amount: 450,
     date: new Date(2021, 5, 12),
   },
@@ -30,12 +28,32 @@ const DUMMY_EXPENSES = [
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
+  const [isLoading, setIsLoading] = useState(false);
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => {
       return [expense, ...prevExpenses];
     });
   };
 
+  const getDataHandler = async () => {
+    setIsLoading(true);
+
+    const resp = await fetch("http://localhost:3000/data");
+    const body = await resp.json();
+
+    console.log(body);
+
+    setExpenses(
+      body.map((item) => ({
+        id: item.myId,
+        title: item.url,
+        amount: `${item.timeTaken} sec`,
+        date: item.time ? new Date(item.time) : new Date(),
+      }))
+    );
+
+    setIsLoading(false);
+  };
   // return React.createElement(
   //   'div',
   //   {},
@@ -45,7 +63,9 @@ const App = () => {
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
+      {isLoading ? "Loading..." : ""}
+      {/* <NewExpense onAddExpense={addExpenseHandler} /> */}
+      <button onClick={getDataHandler}>Get data</button>
       <Expenses items={expenses} />
     </div>
   );
